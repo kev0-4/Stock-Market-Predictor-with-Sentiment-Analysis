87c82201809bf24e3bff188b186f65577587981c
Pratham <pratham07603@gmail.com>
2023-10-27 13:27:34 +0530

    apps

---
+++ stocks/stoc/apps.py
@@ -0,0 +1,12 @@
+# from django.apps import AppConfig
+
+
+# class StocConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'stoc'
+# import   # If using scikit-learn
+
+# def load_ml_model():
+#     model = joblib.load('path_to_your_model.pkl')
+
+#     return model
---
+++ techspark/stocks/stocks/views.py
@@ -0,0 +1,40 @@
+# from django.shortcuts import render
+# import joblib
+
+# def index(request):
+#     return render(request, "index.html")
+
+# def register(request):
+#     return render(request, "register.html")
+
+# def handleRegister(request):
+#     fname = request.POST.get('fname', 'default')
+#     lname = request.POST.get('lname', 'default')
+#     mail = request.POST.get('mail', 'default')
+#     passs = request.POST.get('passs', 'default')
+#     print(fname)
+#     print(lname)
+#     print(mail)
+#     print(passs)
+#     return render(request, "index.html")
+
+# def secScr(request):
+#     return render(request, "secScr.html")
+
+
+# def handlesecScr(request):
+#     stock = request.POST.get('stock', 'default')
+#     print(stock)
+#     # cls = joblib.load("model1.pkl")
+#     # lis = []
+#     # lis.append(request.GET['stock'])
+#     # print(lis)
+#     return render(request, "result.html", {'stock': stock})
+
+
+# def handleLogin(request):
+#     mail = request.POST.get('mail', 'default')
+#     passs = request.POST.get('passs', 'default')
+#     print(mail)
+#     print(passs)
+#     return render(request, "secScr.html")
